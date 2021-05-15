ebaec75b733a61957bf7dbe3f16cf3a2063dba7d
Junya WADA <junya-wada@msen.jp>
2021-05-15 12:06:50 +0900

    クエリパラメータとフォームパラメータを使用

--- app/controllers/start_controller.rb
+++ app/controllers/start_controller.rb
@@ -2,4 +2,14 @@ class StartController < ApplicationController
   def hello()
     render:plain => ('Hello World!')
   end
+
+  def index()
+    if request.post?
+      @title = "Hello Rails!:"
+      @msg = params[:input]
+    else
+      @title = "Hello Rails!:"
+      @msg = "Not POST"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get 'hello' => 'start#hello'
+  get 'index' => 'start#index'
+  post 'index' => 'start#index'
 end
