689fe975781682727ee79766b3e457e6933f9fef
Junya WADA <junya-wada@msen.jp>
2021-06-09 18:12:13 +0900

    ダミーデータを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Person.create(name:'Taro', age:37)
+Person.create(name:'Jiro', age:35)
